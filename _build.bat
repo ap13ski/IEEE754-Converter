8f26b6e9bbeb904df06d11e08715307901e35118
ap13ski <ap13ski@yandex.ru>
2025-03-15 13:46:26 +0300

    Add files via upload

--- _build.bat
+++ _build.bat
@@ -1,6 +1,6 @@
 rem MinGW resource compiler
 windres resources.rc -o resources.o
-g++ main.cpp converter.cpp clipboard.cpp time.cpp file.cpp resources.o -static -lgdi32 -luser32 -lkernel32 -lcomctl32 -lgcc -lstdc++ -mwindows
+g++ main.cpp converter.cpp clipboard.cpp time.cpp file.cpp resources.o -static -lgdi32 -luser32 -lkernel32 -lcomctl32 -lgcc -lstdc++ -mwindows -o ieee754_converter_x64.exe
 
 del *.o
 
